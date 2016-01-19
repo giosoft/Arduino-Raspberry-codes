42220ab0593d6a7bf23b3fdf10c5812c952db1f0
giosoft <you@example.com>
2016-01-19 22:02:02 +0400

    using function

--- LedControlFuntion.py
+++ LedControlFuntion.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import easygui
-
+from LedFunction import LedBasicControl
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.cleanup()
@@ -16,33 +16,6 @@ for LedsLoop in range(1, HowManyLed + 1):
     Leds.append(LedsInput)
     GPIO.setup(Leds[LedsLoop - 1],GPIO.OUT)
 
-def LedBasicControl(LedsNumber,LedPin):
-    print(reply)
-#### ჩართვა
-    if reply  == 1:
-        GPIO.output(Leds[LedsNumber - 1],GPIO.HIGH)
-        print("%s ნათურა აინთო" % LedsNumber)
-#### ციმციმი
-    elif reply == 2:
-        blink = TrueS
-        while blink == True:
-            print("\nციმციმი")
-            replyblink = int(input("1.უხეში ციმციმი\n2.ნაზი ციმციმი\n3.ციმციმის გამორთვა\n"))
-            if replyblink == 1:
-                BlinkNumber = int(input("რამდენჯერ უნდა დაიციმციმოს?\t"))
-                BlinkPerSecond = float(input("რა სიჩქარით იციმციმოს? x დაციმციმება 1 წამში\t"))
-                TimeSleep = 1 / BlinkPerSecond
-                for x in range(BlinkNumberRed):
-                    GPIO.output(Leds[LedsNumber - 1],GPIO.HIGH)
-                    time.sleep(TimeSleep)
-                    GPIO.output(Leds[LedsNumber - 1],GPIO.LOW)
-                    time.sleep(TimeSleep)
-            else:
-                blink = False
-#### გამორთვა
-    elif reply == 3:
-        GPIO.output(Leds[LedsNumber - 1],GPIO.LOW)
-    return;
 
 i = 1
 while i == 1:
