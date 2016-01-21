a5a46719300d4f970935cf31b18e031f4f9cd6eb
giosoft <you@example.com>
2016-01-21 22:14:38 +0400

    finalizing more modern and advanced LedControl with Functions

--- LedControl.py
+++ LedControl.py
@@ -28,13 +28,13 @@ def LedBlinkFunction(LedsNumberFunction):
             while LedFadeLoop == True:
                 LedFadeInput = int(input("\nგსურთ გაგრძელება?\n1.დიახ\n2.არა"))
                 if LedFadeInput == 1:
-                    for FadeDown in range(100)
+                    for FadeDown in range(100):
                         LedFadeDutyCycle = 100
                         LedFade = GPIO.PWM(Leds[0], 200)
                         LedFade.start(redledbrightnessdutycycle)
                         LedFadeDutyCycle -= 1
 
-                    for FadeDown in range(100)
+                    for FadeDown in range(100):
                         LedFade = GPIO.PWM(Leds[0], 200)
                         LedFade.start(redledbrightnessdutycycle)
                         LedFadeDutyCycle += 1
