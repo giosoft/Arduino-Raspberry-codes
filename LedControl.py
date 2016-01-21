3894306b549edd404221d514ad10bf0868ff06fe
giosoft <you@example.com>
2016-01-21 22:13:25 +0400

    finalizing more modern and advanced LedControl with Functions

--- LedControl.py
+++ LedControl.py
@@ -10,6 +10,7 @@ def LedBlinkFunction(LedsNumberFunction):
     while LedBlink == True:
         print("\nციმციმი")
         ReplyLedBlink = int(input("1.უხეში ციმციმი\n2.ნაზი ციმციმი\n3.ციმციმის გამორთვა\n"))
+
         if ReplyLedBlink == 1:
             BlinkNumberLed = int(input("რამდენჯერ უნდა დაიციმციმოს?\t"))
             BlinkPerSecondLed = float(input("რა სიჩქარით იციმციმოს? x დაციმციმება 1 წამში\t"))
@@ -21,6 +22,26 @@ def LedBlinkFunction(LedsNumberFunction):
 
                 GPIO.output(Leds [LedsNumberFunction - 1], GPIO.LOW)
                 time.sleep(TimeSleepLed)
+
+        elif ReplyLedBlink  == 2:
+            LedFadeLoop = True
+            while LedFadeLoop == True:
+                LedFadeInput = int(input("\nგსურთ გაგრძელება?\n1.დიახ\n2.არა"]))
+                if LedFadeInput == 1:
+                    for FadeDown in range(100)
+                        LedFadeDutyCycle = 100
+                        LedFade = GPIO.PWM(Leds[0], 200)
+                        LedFade.start(redledbrightnessdutycycle)
+                        LedFadeDutyCycle -= 1
+
+                    for FadeDown in range(100)
+                        LedFade = GPIO.PWM(Leds[0], 200)
+                        LedFade.start(redledbrightnessdutycycle)
+                        LedFadeDutyCycle += 1
+                else:
+                    LedFade.stop()
+                    LedFadeLoop = False
+
         else:
             LedBlink = False
 
