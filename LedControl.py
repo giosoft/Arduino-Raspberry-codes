a6a383aa644cbae6397172bb443d1d493d975a91
giosoft <you@example.com>
2016-01-24 18:03:32 +0400

    fixing bugs

--- LedControl.py
+++ LedControl.py
@@ -88,7 +88,7 @@ while i == 1:
         strMenuListNumberThree = str(MenuListNumberThree)
 
         MenuListString = strMenuListNumberOne + ". " + strMenuListLedNumber + " ნათურა - ჩართვა\n" + strMenuListNumberTwo + ". " + strMenuListLedNumber + " ნათურა - ციმციმი\n" + strMenuListNumberThree + ". " + strMenuListLedNumber + " ნათურა - გამორთვა\n\n"
-        MenuList.insert(MenuListIndex, 123)
+        MenuList.insert(MenuListIndex, MenuList)
 
         MenuListLedNumber += 1
         MenuListIndex += 1
